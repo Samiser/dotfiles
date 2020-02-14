78c64b6903840cfe6d4c755feb5e5e5f9bd44192
Samiser <sheney10@gmail.com>
2020-02-14 01:52:28 +0000

    changed terminal to alacritty

--- scripts/.scripts/terminal_present.sh
+++ scripts/.scripts/terminal_present.sh
@@ -1,3 +1,1 @@
-xrdb -merge ~/.scripts/presfont 
-urxvtc &
-xrdb -merge ~/.Xresources
+alacritty --config-file <(echo -ne 'window:\n  padding:\n    x: 15\n    y: 15\n\nfont:\n  size: 10\n  normal:\n    family: DejaVu Sans Mono\n\nbackground_opacity: 0.95')
