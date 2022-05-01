76a71ee4b9e592a29ed6cb17b6d77a796e4703bf
Samiser <sheney10@gmail.com>
2022-05-01 13:53:16 +0100

    deleted pywal cache

--- wal-cache/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c6dedc", "#0F3C46" },
-	[SchemeSel] = { "#c6dedc", "#2193AE" },
-	[SchemeOut] = { "#c6dedc", "#C4CEAD" },
-};
--- wal-cache/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#0F3C46";
-static const char* selfgcolor   = "#c6dedc";
-static const char* normbgcolor  = "#5F9DA6";
-static const char* normfgcolor  = "#c6dedc";
-static const char* urgbgcolor   = "#2193AE";
-static const char* urgfgcolor   = "#c6dedc";
